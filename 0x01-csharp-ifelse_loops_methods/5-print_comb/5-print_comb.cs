25a48bad6a54c37c043f03da8859fc796fc60937
Virteip <sergiopietri@gmail.com>
2020-07-15 02:49:26 +0000

    [Add]

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 0; number <= 99; number++){
+	    	if (number != 0){
+		   Console.Write(", {0}", number);
+		} else {
+		   Console.Write("{0}", number);
+		}
+	    }
+        }
+    }
+}
