a4464f8e8bf1aaf19ddc9e4a73adb2ab4b080150
Virteip <sergiopietri@gmail.com>
2020-07-15 02:53:15 +0000

    [Fix] output double digit

--- 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -8,9 +8,9 @@ namespace _5_print_comb
         {
             for (int number = 0; number <= 99; number++){
 	    	if (number != 0){
-		   Console.Write(", {0}", number);
+		   Console.Write(", {0:d2}", number);
 		} else {
-		   Console.Write("{0}", number);
+		   Console.Write("{0:d2}", number);
 		}
 	    }
         }
