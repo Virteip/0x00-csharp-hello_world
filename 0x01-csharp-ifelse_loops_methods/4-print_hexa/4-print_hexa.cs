98bce957bf294dc76fabbdf920658d3ba1eee10c
Virteip <sergiopietri@gmail.com>
2020-07-15 02:27:33 +0000

    [Add]

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+		for (int num = 0; num <= 98; num++)
+	    	{
+			Console.WriteLine("{0} = 0x{0:x}", num);
+	    	}
+        }
+    }
+}
