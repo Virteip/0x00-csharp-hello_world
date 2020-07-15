ec4998796ba9956afa8731ec66a08c2c99ca862d
Virteip <sergiopietri@gmail.com>
2020-07-15 04:51:50 +0000

    [Add]

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             for (int number = 1; number <= 99; number++)
+            {
+                if (number % 15 == 0){
+                    	Console.Write("FizzBuzz ");
+		    }
+                else if (number % 5 == 0){
+                     	Console.Write("Buzz ");
+		    }
+                else if (number % 3 == 0){
+                     	Console.Write("Fizz ");
+		    }
+                else{
+			Console.Write("{0} ", number);
+		    }
+            }
+            Console.Write("Buzz");
+        }
+    }
+}
