beadce70d6b338e2378c0f7200b54004e93b7e83
Virteip <sergiopietri@gmail.com>
2020-07-15 04:31:12 +0000

    [Add]

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+    class Line
+    {
+        public static void PrintLine(int length)
+        {
+	    int counter = 1;
+
+	    while (counter <= length){
+
+	    	  Console.Write("_");
+		  counter++;
+	    }
+	    Console.WriteLine();
+        }
+    }
