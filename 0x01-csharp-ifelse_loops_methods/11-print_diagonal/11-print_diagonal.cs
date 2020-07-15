d8ce7f12a7ae3e67fe730552ac39d5c3dbd26ce0
Virteip <sergiopietri@gmail.com>
2020-07-15 04:42:43 +0000

    [Add]

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+    class Line
+    {
+        public static void PrintDiagonal(int length)
+        {
+           for (int row = 0; row < length; row++)
+           {
+		 for (int space = 0; space < row; space++){
+                     	  Console.Write(" ");
+		 }
+            	 Console.WriteLine("\\");
+           }
+		Console.WriteLine("");
+        }
+    }
