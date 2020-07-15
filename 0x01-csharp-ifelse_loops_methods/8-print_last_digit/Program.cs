75a00ce0609f705bc0274b408c081eaa61ede3b4
Virteip <sergiopietri@gmail.com>
2020-07-15 04:13:09 +0000

    [Add]

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+    class Number
+    {
+        public static int PrintLastDigit(int number)
+        {
+	    int number_absolute = number % 10 > 0 ? (number % 10) : (number%10 * -1);
+            Console.Write("{0}", number_absolute);
+	    return (number_absolute);
+        }
+    }
