5c331f4a1597a5c579df673426cc25ff94a0a03a
Virteip <sergiopietri@gmail.com>
2020-07-15 03:10:16 +0000

    [Add]

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             for (int num_1 = 0; num_1 < 10; num_1++)
+            {
+                for (int num_2 = num_1 + 1 ; num_2 < 10; num_2++)
+                    if (num_1 != num_2)
+                    {
+                        Console.Write("{0:d}", num_1);
+                        if (num_1 == 8)
+                            Console.WriteLine("{0}", num_2);
+                        else
+                            Console.Write("{0:d}, ", num_2);
+                    }
+            }
+        }
+    }
+}
