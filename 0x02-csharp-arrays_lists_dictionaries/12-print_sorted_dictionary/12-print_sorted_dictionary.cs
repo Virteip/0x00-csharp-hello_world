3b4ba7ca69e56bd0bf278493d164d4cbe1e71196
Virteip <sergiopietri@gmail.com>
2020-07-16 23:25:13 +0000

    [Add]

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Dictionary
+    {
+        public static void PrintSorted(Dictionary<string, string> myDict) 
+        {
+            List<string> keylist = new List<string>();
+
+            foreach(var row in myDict){
+                keylist.Add(row.Key);
+            }
+            keylist.Sort();
+            
+            for(int i = 0; i < keylist.Count; i++){
+                string key = keylist[i];
+                Console.WriteLine($"{key}: {myDict[key]}");
+            }
+        }
+    }
+
