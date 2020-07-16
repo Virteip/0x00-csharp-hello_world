c667cfeb8ed55f6c81a595aadeb1b9319c50026f
Virteip <sergiopietri@gmail.com>
2020-07-16 21:26:52 +0000

    [Add]

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List 
+    {
+        public static List<bool> DivisibleBy2(List<int> myList)
+        {
+                List<bool> mod2 = new List<bool>();
+
+                foreach (int item in myList)
+                    if(item % 2 == 0){
+                        mod2.Add(true);
+                    }else{
+                        mod2.Add(false);
+                    }
+                
+                return mod2;
+        }
+    }
+
