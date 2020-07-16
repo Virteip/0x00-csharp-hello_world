9004fcee962e2cd9a516720c3ab143cbb7277993
Virteip <sergiopietri@gmail.com>
2020-07-16 17:55:58 +0000

    [Add] Initialize folder

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+    class Array
+    {
+        public static int[] CreatePrint(int size)
+        {
+            if (size >= 0){
+	       int[] array = new int[size];
+	       for (int i = 0; i < size; i++){
+	       	   array[i] = i;
+		   Console.Write($"{array[i]} ");
+	       }
+	       Console.WriteLine();
+	       return (array);
+	    } else {
+		Console.WriteLine("Size cannot be negative");
+		return (null);
+	    }
+        }
+   }
