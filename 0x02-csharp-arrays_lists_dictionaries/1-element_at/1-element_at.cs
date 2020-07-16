7a5586dbbbb17269dc58f450e11487bef16f4a18
Virteip <sergiopietri@gmail.com>
2020-07-16 18:03:08 +0000

    [Add]

---
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+    class Array
+    {
+        public static int elementAt(int[] array, int index)
+	{
+		if (index <= array.Length-1 && index >= 0 ){
+			return (array[index]);
+		} else {
+			Console.WriteLine("Index out of range");
+			return (-1);
+		}
+        }
+    }
+
