6532659b6816196c09024e206f46e6b39ae6be93
Virteip <sergiopietri@gmail.com>
2020-07-16 20:58:17 +0000

    [Add]

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static int MaxInteger(List<int> myList)
+	    {
+            Console.WriteLine($"{myList.Count}");
+            if(myList.Count > 0){
+                myList.Sort();
+                int last = myList.Count - 1;
+
+	   	        return myList[last];
+	        } else {
+                Console.WriteLine("List is empty");
+                return -1;
+            }
+        }
+    }
