91cea00ca6566163030f2264a473324768cd4b56
Virteip <sergiopietri@gmail.com>
2020-07-16 20:07:54 +0000

    [Fix] switch conditions

--- 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -4,11 +4,11 @@
     {
         public static int[] ReplaceElement(int[] array, int index, int n)
 	{
-		if (index < array.Length - 1 && index >= 0){
-			array[index] = n;	
+		if (index > array.Length - 1 < index < 0){
+			Console.WriteLine("Index out of range");
 			return (array);
 		} else {
-			Console.WriteLine("Index out of range");
+			array[index] = n;	
 			return(array);
 		}
         }
