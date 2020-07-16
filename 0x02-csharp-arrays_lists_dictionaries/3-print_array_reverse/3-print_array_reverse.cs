72c5056115a71c985a38b5819f9c9d605778984b
Virteip <sergiopietri@gmail.com>
2020-07-16 20:13:41 +0000

    [Add]

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+    class Array
+    {
+           public static void Reverse(int[] array)
+	   {
+	      if (array != null){
+        	for (int point = array.Length - 1; point >= 0; point--) {
+            	   Console.Write(array[point] + " ");
+           	}
+              }
+	      Console.WriteLine();
+    	   }
+    }
+
