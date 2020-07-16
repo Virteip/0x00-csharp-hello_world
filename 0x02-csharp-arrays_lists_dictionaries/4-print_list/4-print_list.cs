2041aa53a652c990a888704afbdb17addfbe22c6
Virteip <sergiopietri@gmail.com>
2020-07-16 20:26:30 +0000

    [Add]

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+    class List 
+    {
+        public static List<int> CreatePrint(int size)
+	{
+            if (size < 0){
+		Console.WriteLine("Size cannot be negative");
+		return (null);
+	    } else {
+	    	List<int> lista = new List<int>();
+		for (int nums = 0; nums <= size - 1; nums++){
+			lista.Add(nums);
+			if (nums == size - 1){
+			    Console.Write($"{nums}");
+			    break;
+			}
+			Console.Write($"{nums} ");
+		}
+		Console.Write("\n");
+	    	return(lista);
+	    }
+        }
+    }
