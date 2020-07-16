c045a004a3a41d5a7b50e468b56d76990872c08a
Virteip <sergiopietri@gmail.com>
2020-07-16 21:58:26 +0000

    [Add]

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class Dictionary
+    {
+        public static int NumberOfKeys(Dictionary<string, string> myDict)
+        {   
+            int count = 0;
+            foreach (KeyValuePair<string, string> key in myDict){
+                count += 1;
+            }
+            return count;
+        }
+    }
+
