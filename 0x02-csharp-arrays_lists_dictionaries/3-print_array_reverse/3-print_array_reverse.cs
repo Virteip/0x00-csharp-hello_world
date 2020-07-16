40d5ced923d7d13b59c39850c341054ef8278a82
Virteip <sergiopietri@gmail.com>
2020-07-16 20:18:38 +0000

    [Update] condition

--- 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -6,7 +6,10 @@
 	   {
 	      if (array != null){
         	for (int point = array.Length - 1; point >= 0; point--) {
-            	   Console.Write(array[point] + " ");
+			Console.Write($"{array[point]}");
+			if (point != array.Length-1){
+				Console.Write(" ");
+		   	}
            	}
               }
 	      Console.WriteLine();
