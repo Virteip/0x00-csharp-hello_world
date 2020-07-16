80ab61b0551133c8d025d4e7c3bfb7a17c152e66
Virteip <sergiopietri@gmail.com>
2020-07-16 20:05:30 +0000

    [Update] output

--- 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -4,7 +4,7 @@
     {
         public static int[] ReplaceElement(int[] array, int index, int n)
 	{
-		if (index < array.Length - 1 || index >= 0){
+		if (index < array.Length - 1 && index >= 0){
 			array[index] = n;	
 			return (array);
 		} else {
