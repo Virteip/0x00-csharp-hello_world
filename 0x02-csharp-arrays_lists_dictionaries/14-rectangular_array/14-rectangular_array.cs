c7b79b9edf91fe03f8b4184c29a4dcfa0ab69009
Virteip <sergiopietri@gmail.com>
2020-07-17 01:31:36 +0000

    [Add]

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] grid = new int[5,5];
+            grid[2,2] = 1;
+
+            for (int i = 0; i < 5; i++){
+                for (int j = 0; j < 5; j++){
+                    Console.Write(grid[i,j]);
+                    if(j != 5 - 1){
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
