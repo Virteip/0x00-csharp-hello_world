4a4d6dcb49d95919f60b9d37ad3ed780167c53c0
Virteip <sergiopietri@gmail.com>
2020-07-17 01:58:45 +0000

    [Add]

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+    class Matrix
+    {
+        public static int[,] Square(int[,] myMatrix)
+        {   
+            int width = myMatrix.GetLength(0);
+            int height = myMatrix.GetLength(1);
+            int[,] grid = new int[width,height];
+
+            for (int i = 0; i < width; i++){
+                for (int j = 0; j < height; j++){
+                    grid[i,j] = myMatrix[i, j] * myMatrix[i, j];
+                }
+            }
+            return grid;
+        }
+    }
+
