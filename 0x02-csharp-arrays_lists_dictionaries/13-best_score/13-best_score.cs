051402b69a6c61b14bede20a89c3387643515a6a
Virteip <sergiopietri@gmail.com>
2020-07-17 00:43:20 +0000

    [Add]

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Dictionary
+    {
+        public static string BestScore(Dictionary<string, int> myList)
+        {
+            int max = 0;
+            string name = null;
+            foreach(KeyValuePair<string, int> pair in myList){
+                    if(max < pair.Value){
+                        max = pair.Value;
+                        name = pair.Key;
+                    }
+            }
+            return name;
+        }
+    }
+
