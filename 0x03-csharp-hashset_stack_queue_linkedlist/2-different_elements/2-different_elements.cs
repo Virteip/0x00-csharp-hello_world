a526f2cbd27ce548d6c70d3f52363d55488bacfe
Virteip <sergiopietri@gmail.com>
2020-07-27 15:03:37 -0500

    [Add]

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2) 
+    {
+        List<int> newList = new List<int>();
+        foreach (int item in list1){
+          	if(!list2.Contains(item)){
+				newList.Add(item);
+			}
+		}
+
+		foreach (int item in list2){
+			if(!list1.Contains(item)){
+				newList.Add(item);
+			}
+		}
+
+		newList.Sort();
+
+		return newList;
+    }
+}
