abe61a541b27c83f69b44ea0b2d5009e1a05a27a
Virteip <sergiopietri@gmail.com>
2020-07-27 15:42:05 -0500

    [Add]

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+class MyStack 
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+		bool found = aStack.Contains(search);
+
+		if(aStack.Count != 0){
+			Console.Write($"Number of items: {aStack.Count}\nTop of items: {aStack.Peek()}\nStack contains \"{search}\": {found}\n");
+			
+			if(found){
+				while(aStack.Pop() != search){
+					continue;
+				}
+			}
+		}else{
+			Console.WriteLine("Stack is empty");
+		}
+
+		aStack.Push(newItem);
+
+		return (aStack);
+    }
+}
