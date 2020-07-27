b29a8157f782eba0723327cb2866bf941fb65d97
Virteip <sergiopietri@gmail.com>
2020-07-27 17:01:45 -0500

    [Add]

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+		int length = 0;
+        foreach (var item  in myLList)
+            length++;
+        return (length);       	 
+    }
+}
