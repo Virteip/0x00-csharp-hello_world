d6bb9fbdcef69ade63e74ba7da65ce7aea6d028a
Virteip <sergiopietri@gmail.com>
2020-07-27 19:38:58 -0500

    [Add]

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int result = 0;
+        foreach (int number in myLList)
+        {
+            result = number + result;
+        }
+        return (result);
+    }
+}
