1f2216f8001bec852fe7390a873ddd20a5350db6
Virteip <sergiopietri@gmail.com>
2020-07-30 17:54:51 -0500

    [Update] add comment on field

--- 0x06-csharp-classes/1-enemy/1-enemy.cs
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -5,6 +5,7 @@ namespace Enemies
     ///<summary>Create public Zombie Class</summary>
         public class Zombie
         {
+    ///<summary>Heatlh field.</summary>
           public int health;
     ///<summary>Zombie constructor.</summary>
           public Zombie(){
