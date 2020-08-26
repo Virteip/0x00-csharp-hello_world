fdf55528ba41607b6c05d1e648b338acef65e69a
Virteip <sergiopietri@gmail.com>
2020-08-26 18:55:13 -0500

    [Add]

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.Length == 2)
+        {	
+			double[] res = Math.Round(Math.Sqrt(vector[0]*vector[0]+vector[1]*vector[1]), 2);
+			return res;
+        }
+		
+        if (vector.Length == 3)
+        {
+            return (Math.Round(Math.Sqrt(vector[0]*vector[0]+vector[1]*vector[1]+vector[2]*vector[2]), 2));
+        }
+
+        return (-1);
+    }
+}
